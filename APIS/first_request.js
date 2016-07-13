7ee392248a2e481442a20a544061d6d748629f58
Jordan S <goglobalpioneers@gmail.com>
2016-07-13 19:41:43 -0400

    updated first_request.js

--- APIS/first_request.js
+++ APIS/first_request.js
@@ -1,9 +1,4 @@
-// var request = require('request');
-// request('http://www.google.com', function (error, response, body) {
-//   if (!error && response.statusCode == 200) {
-//     console.log(body) // Show the HTML for the Google homepage.
-//   }
-// })
+
 
 var request = require('request');
 
